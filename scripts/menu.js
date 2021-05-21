6fcaa1fa340e732a3604e0ccb78e23a984e51129
Nejah Al-mawla <nejah.al-mawala@etu.u-bordeaux.fr>
2021-05-21 14:04:59 +0200

    first commit

---
+++ scripts/menu.js
@@ -0,0 +1,28 @@
+function openChoice(evt , the_choice){
+    var i , choices_content, choices ;
+
+    choices_content = document.getElementsByClassName("choices-content");
+    for (i = 0 ; i < choices_content.length;i++){
+        choices_content[i].style.display = "none";
+    }
+
+    choices = document.getElementsByClassName("choices");
+    for (i = 0 ; i < choices_content.length;i++){
+        choices[i].className = choices[i].className.replace(" active","");
+    }
+
+    document.getElementById(the_choice).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "200px";
+    document.getElementById("main").style.marginLeft = "200px";
+}
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
