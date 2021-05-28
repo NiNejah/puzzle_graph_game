ed19e619c3ad7ed139a5515e169e5b286fadae39
Nejah Al-mawla <nejah.al-mawala@etu.u-bordeaux.fr>
2021-05-28 10:05:44 +0200

    little change in graph.js

--- scripts/graph.js
+++ scripts/graph.js
@@ -1,10 +1,10 @@
-
 let cy = cytoscape({
-    container: document.getElementById('cy') ,// container to render in
+    container: document.getElementById('cy'),// container to render in
+
     elements: [
-        {data: {id: 'a' }},
-        {data: {id: 'b' }},
-        {data:{id:'ab', source:'a',target:'b'}}
+        {data: {id: 'a'}},
+        {data: {id: 'b'}},
+        {data: {id: 'ab', source: 'a', target: 'b'}}
     ],
 
     layout: {
@@ -15,15 +15,43 @@ let cy = cytoscape({
     style: cytoscape.stylesheet()
         .selector('node')
         .css({
-            'background-color':'#ff7d7d',
-            'label':'data(id)'
+            'type': 'barrel',
+            'background-color': '#ff7d7d',
+            'label': 'data(id)'
         })
         .selector('edge')
         .css({
-            'width': 3 ,
-            'line-color':'#ccc',
+            'width': 3,
+            'line-color': '#ccc',
             'target-arrow-shape': 'triangle',
             'curve-style': 'bezier'
-        })
+        }),
+    pan: {x: 10, y: 10},
 
+});
+
+let eles = cy.add([
+    {
+        group: 'nodes',
+        data: {id: 'c'},
+        position: {x: 700 , y: 200}
+    },
+    {
+        group:'edges',
+        data:{id:'bc',source: 'b',target: 'c'}
+    },
+    {
+        group:'edges',
+        data:{id:'cb',source: 'c',target: 'b'}
+    }
+]);
+
+// test :
+// let collection = cy.collection();
+cy.nodes().on('click', function(e){
+    let clickedNode = e.target;
+    console.log("clickedNode : ",clickedNode);
+    cy.remove(clickedNode);
+    // collection = collection.union(clickedNode);
+    // console.log("collection : ",collection);
 });
