57bd8c6698bca7c67c9df73bbde2da1d229a2ba8
Nejah Al-mawla <nejah.al-mawala@etu.u-bordeaux.fr>
2021-05-31 12:13:11 +0200

    add image with a bug!

--- scripts/file.js
+++ scripts/file.js
@@ -1,16 +1,16 @@
 let file = document.getElementById("myfiles");
-console.log("file : " , file);
+// console.log("file : ", file);
 let columns = document.querySelectorAll(".column");
 
-file.onchange =(()=>{
-    arrImg =[];
-    for (const key in file.files){
-        if(file.files.hasOwnProperty(key)){
+file.onchange = (() => {
+    arrImg = [];
+    for (const key in file.files) {
+        if (file.files.hasOwnProperty(key)) {
             const element = file.files[key];
-            if (element.type == "image/jpeg" || element.type == "image/png"){
+            if (element.type == "image/jpeg" || element.type == "image/png") {
                 let parent_column = minParent(columns);
                 let myimg = makeImag(element.webkitRelativePath);
-                arrImg[key]= myimg ;
+                arrImg[key] = myimg;
                 parent_column.appendChild(myimg);
             }
         }
@@ -18,77 +18,79 @@ file.onchange =(()=>{
 });
 
 // pour trouver le colonne qui contient le minumeme de image
-function minParent(parentNode){
+function minParent(parentNode) {
     let arry = [];
-    parentNode.forEach((element,i) => {
+    parentNode.forEach((element, i) => {
         arry[i] = element.children.length;
     });
-    let min = Math.min.apply(null,arry);
-    for (let i = 0 ; i < parentNode.length ; i++){
-        if (parentNode[i].children.length == min){
+    let min = Math.min.apply(null, arry);
+    for (let i = 0; i < parentNode.length; i++) {
+        if (parentNode[i].children.length == min) {
             return parentNode[i];
         }
     }
 }
 
-function makeImag(imgsrc){
+function makeImag(imgsrc) {
     let img = document.createElement("img");
-    img.setAttribute("src",imgsrc);
+    img.setAttribute("src", imgsrc);
     img.className = "myImg";
-    return img ;
+    return img;
 }
 
 // to select :
-function on_select(){
+function on_select() {
     let imgs = document.querySelectorAll(".column img");
-    console.log("imgs : " , imgs);
-    if (imgs.length === 0) return false ;
+    // console.log("imgs : ", imgs);
+    if (imgs.length === 0) return false;
     else {
-        for (let i = 0 ; i < imgs.length;i++){
+        for (let i = 0; i < imgs.length; i++) {
             imgs[i].onclick = () => {
-                if (imgs[i].className === "selectedImg"){
+                if (imgs[i].className === "selectedImg") {
                     imgs[i].className = "myImg";
-                }else {
+                } else {
                     imgs[i].className = "selectedImg";
                 }
             }
         }
     }
-    return true ;
+    return true;
 }
 
-function selectAll(){
+function selectAll() {
     let imgs = document.querySelectorAll(".column img");
     imgs.forEach(element => {
         element.className = "selectedImg";
     });
 }
-function deselectAll(){
+
+function deselectAll() {
     let imgs = document.querySelectorAll(".column img");
     imgs.forEach(element => {
         element.className = "myImg";
     });
 }
-function displayImgs(){
+
+function displayImgs() {
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext('2d');
     var slectedImgs = document.getElementsByClassName("selectedImg");
-    let x = 0 ;
-    let y = 0 ;
-    let img ;
-    if (slectedImgs.length === 0) return false ;
-    for (let i= 0 ; i < slectedImgs.length ; i ++){
+    let x = 0;
+    let y = 0;
+    let img;
+    if (slectedImgs.length === 0) return false;
+    for (let i = 0; i < slectedImgs.length; i++) {
         img = slectedImgs[i];
         let myImg = new Image();
         myImg.src = img.attributes[0].value;
-        ctx.drawImage(myImg, x, y ,50,50);
+        ctx.drawImage(myImg, x, y, 50, 50);
         x += 50;
-        if ( x >= c.width ){
-            x = 0 ;
-            y +=50;
+        if (x >= c.width) {
+            x = 0;
+            y += 50;
         }
     }
-    return true ;
+    return true;
 }
 
 
--- scripts/graph.js
+++ scripts/graph.js
@@ -1,11 +1,11 @@
 let cy = cytoscape({
     container: document.getElementById('cy'),// container to render in
 
-    elements: [
-        {data: {id: 'a'}},
-        {data: {id: 'b'}},
-        {data: {id: 'ab', source: 'a', target: 'b'}}
-    ],
+    // elements: [
+    //     {data: {id: 'a'}},
+    //     {data: {id: 'b'}},
+    //     {data: {id: 'ab', source: 'a', target: 'b'}}
+    // ],
 
     layout: {
         name: 'grid',
@@ -15,9 +15,13 @@ let cy = cytoscape({
     style: cytoscape.stylesheet()
         .selector('node')
         .css({
-            'type': 'barrel',
-            'background-color': '#ff7d7d',
-            'label': 'data(id)'
+            'height': 80,
+            'width': 80,
+            'background-fit': 'cover',
+            'lable':'data(id)',
+            'border-color': '#8ce8ff',
+            'border-width': 3,
+            'border-opacity': 0.5
         })
         .selector('edge')
         .css({
@@ -30,28 +34,51 @@ let cy = cytoscape({
 
 });
 
-let eles = cy.add([
-    {
-        group: 'nodes',
-        data: {id: 'c'},
-        position: {x: 700 , y: 200}
-    },
-    {
-        group:'edges',
-        data:{id:'bc',source: 'b',target: 'c'}
-    },
-    {
-        group:'edges',
-        data:{id:'cb',source: 'c',target: 'b'}
-    }
-]);
+// let eles = cy.add([
+//     {
+//         group: 'nodes',
+//         data: {id: 'c'},
+//         position: {x: 700, y: 200}
+//     },
+//     {
+//         group: 'edges',
+//         data: {id: 'bc', source: 'b', target: 'c'}
+//     },
+//     {
+//         group: 'edges',
+//         data: {id: 'cb', source: 'c', target: 'b'}
+//     }
+// ]);
 
 // test :
 // let collection = cy.collection();
-cy.nodes().on('click', function(e){
-    let clickedNode = e.target;
-    console.log("clickedNode : ",clickedNode);
-    cy.remove(clickedNode);
-    // collection = collection.union(clickedNode);
-    // console.log("collection : ",collection);
-});
+// cy.nodes().on('click', function(e){
+//     let clickedNode = e.target;
+//     console.log("clickedNode : ",clickedNode);
+//     cy.remove(clickedNode);
+//     // collection = collection.union(clickedNode);
+//     // console.log("collection : ",collection);
+// });
+let nb_img = 0 ;
+function addImag() {
+    var slectedImgs = document.getElementsByClassName("selectedImg");
+    let css_vars;
+    const getVar = (variable) => (css_vars ? css_vars.getVar(variable) : null)
+    for (let i = nb_img; i < nb_img+slectedImgs.length; i++) {
+        // console.log("src e : ", slectedImgs[i].src);
+        let e = i + 1;
+        cy.add({
+            group: 'nodes',
+            data: {id: e},
+            position: {x: 200, y: 200}
+        });
+        cy.style()
+            .selector('#'+toString(e))
+            .style({
+                'background-image': 'url('+slectedImgs[i].src+')'
+            })
+            .update()
+        ;
+        console.log("les nodes : " ,cy.data.id);
+    }
+}
